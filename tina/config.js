787af567dac8c8c6da5a6f9caf87d4c5eb81f34c
Meirunas Smitas <45215244+Meirunas@users.noreply.github.com>
2024-12-07 11:42:42 +0000

    add a new project

--- tina/config.js
+++ tina/config.js
@@ -1,114 +1,140 @@
 import { defineConfig } from "tinacms";
-require('dotenv').config({ path: '.env.development' });
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || "main";
 
 export default defineConfig({
-    branch,
-    clientId: process.env.TINACLIENTID, // Get this from tina.io
-    token: process.env.TINATOKEN, // Get this from tina.io
+	branch,
+	clientId: "9ff6470c-6454-47c7-8924-1d0bb29a079a", // Get this from tina.io
+	token: "340c5349ada4e18f48e37c69d47c3f528a6a2a6c", // Get this from tina.io
 
-    build: {
-        outputFolder: "admin",
-        publicFolder: "public",
-    },
-
-    media: {
-        tina: {
-            mediaRoot: "assets",
-            publicFolder: "public",
-        },
-    },
-
-    schema: {
-        collections: [
-            {
-                name: "post",
-                label: "Posts",
-                path: "src/posts",
-                defaultItem: () => ({
-                    title: "New Post",
-                    layout: "../layouts/BlogPost.astro",
-                    added: new Date(),
-                    tags: [],
-                }),
-                ui: {
-                    dateFormat: "MMM DD YYYY",
-                    filename: {
-                        readonly: false,
-                        slugify: (values) => values?.slug?.toLowerCase().replace(/ /g, "-"),
-                    },
-                },
-                fields: [
-                    {
-                        name: "layout",
-                        label: "Layout",
-                        type: "string",
-                        required: true,
-                        searchable: false,
-                    },
-                    {
-                        name: "title",
-                        label: "Title",
-                        type: "string",
-                        isTitle: true,
-                        required: true,
-                    },
-                    {
-                        label: "Slug",
-                        name: "slug",
-                        type: "string",
-                        required: true,
-                    },
-                    {
-                        label: "Description",
-                        name: "description",
-                        type: "string",
-                        required: true,
-                    },
-                    {
-                        label: "Tags",
-                        name: "tags",
-                        type: "list",
-                        ui: {
-                            component: "tags",
-                        },
-                        options: [
-                            { value: "technical", label: "Technical" },
-                            { value: "personal", label: "Personal" },
-                        ],
-                    },
-                    {
-                        label: "Added",
-                        name: "added",
-                        type: "datetime",
-                        dateFormat: "MMM DD YYYY",
-                        required: true,
-                    },
-                    {
-                        label: "Updated",
-                        name: "updated",
-                        type: "datetime",
-                        dateFormat: "MMM DD YYYY",
-                    },
-                    {
-                        type: "rich-text",
-                        name: "body",
-                        label: "Body",
-                        isBody: true,
-                    },
-                ],
-            },
-        ],
-    },
-
-    search: {
-        tina: {
-            indexerToken: process.env.TINASEARCH,
-            stopwordLanguages: ["eng"],
-        },
-        indexBatchSize: 50,
-        maxSearchIndexFieldLength: 100,
-    },
+	build: {
+		outputFolder: "admin",
+		publicFolder: "public",
+	},
+	media: {
+		tina: {
+			mediaRoot: "assets",
+			publicFolder: "public",
+		},
+	},
+	schema: {
+		collections: [
+			{
+				name: "post",
+				label: "Posts",
+				path: "src/posts",
+				defaultItem: () => ({
+					title: "New Post",
+					layout: "../layouts/BlogPost.astro",
+					added: new Date(),
+					tags: [],
+				}),
+				ui: {
+					dateFormat: "MMM DD YYYY",
+					filename: {
+						readonly: false,
+						slugify: (values) => {
+							return values?.slug?.toLowerCase().replace(/ /g, "-");
+						},
+					},
+				},
+				fields: [
+					{
+						name: "layout",
+						label: "Layout",
+						type: "string",
+						required: true,
+						searchable: false,
+					},
+					{
+						name: "title",
+						label: "Title",
+						type: "string",
+						isTitle: true,
+						required: true,
+					},
+					{
+						label: "Slug",
+						name: "slug",
+						type: "string",
+						required: true,
+					},
+					{
+						label: "Description",
+						name: "description",
+						type: "string",
+						required: true,
+					},
+					{
+						label: "Tags",
+						name: "tags",
+						type: "string",
+						list: true,
+						options: [
+							{
+								value: "technical",
+								label: "Technical",
+							},
+							{
+								value: "advice",
+								label: "Advice",
+							},
+							{
+								value: "events",
+								label: "Events",
+							},
+							{
+								value: "learning",
+								label: "Learning",
+							},
+							{
+								value: "meta",
+								label: "Meta",
+							},
+							{
+								value: "work",
+								label: "Work",
+							},
+							{
+								value: "personal",
+								label: "Personal",
+							},
+							{
+								value: "musings",
+								label: "Musings",
+							},
+						],
+					},
+					{
+						label: "Added",
+						name: "added",
+						type: "datetime",
+						dateFormat: "MMM DD YYYY",
+						required: true,
+					},
+					{
+						label: "Updated",
+						name: "updated",
+						type: "datetime",
+						dateFormat: "MMM DD YYYY",
+					},
+					{
+						type: "rich-text",
+						name: "body",
+						label: "Body",
+						isBody: true,
+					},
+				],
+			},
+		],
+	},
+	search: {
+		tina: {
+			indexerToken: "72978149db8ba089595c6fc425efaeec01494e7f",
+			stopwordLanguages: ["eng"],
+		},
+		indexBatchSize: 50,
+		maxSearchIndexFieldLength: 100,
+	},
 });
